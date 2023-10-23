717fd245673ddf4bb83c938ed779dde79db31328
fawwazjibran <134182460+fawwazjibran@users.noreply.github.com>
2023-10-23 16:36:40 +0700

    Add files via upload

---
+++ latihan1b.php
@@ -0,0 +1,18 @@
+<?php
+
+$x = 10;
+echo "Aku adalah angka $x </br>";
+
+$hasilKali = $x * 8;
+echo "jika aku di bagi 8, jumlahku sekarang $hasilKali </br>";
+
+$hasilBagi = $hasilKali / 4;
+echo "jika aku di kurang 6, jumlahku sekarang $hasilBagi </br>";
+
+$hasilKurang = $hasilBagi -6 ;
+echo "jika aku di tambah 2, jumlahku sekarang $hasilKurang </br>";  
+
+$hasilTambah = $hasilKurang + 2;
+echo "jika aku di tambah 2, jumlahku sekarang $hasilTambah </br>"
+
+?>
